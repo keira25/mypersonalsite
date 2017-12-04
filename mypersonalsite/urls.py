d04bfb68f7e702dc62e87898469bb587f85ced44
keira25 <Keira1425@gmail.com>
2017-12-05 08:08:00 +0900

    Just small changes

--- mypersonalsite/urls.py
+++ mypersonalsite/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('mypersonal.urls')),
+    url(r'^photos/', include('myphotos.urls')),
 ]
