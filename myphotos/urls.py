0a8b103301ea47df6e4ce288e475cf818d82f3d1
keira25 <Keira1425@gmail.com>
2017-12-05 08:22:10 +0900

    Gotta go to work

--- myphotos/urls.py
+++ myphotos/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^photos/', views.index, name='home'),
+    url(r'^$', views.photos, name=''),
 ]
--- myphotos/views.py
+++ myphotos/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def photos(request):
+    return render(request, 'photos/photos.html')
